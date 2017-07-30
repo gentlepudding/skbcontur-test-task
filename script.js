15742a511d12e0576fc99aa269247ef01c063f77
Maria Paradeeva <gentlepudding@gmail.com>
2017-07-30 23:54:21 +0500

     BEM, remove comments, bug fixes

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function createCitiesList(entities) {
         const city = entity.City;
         const li = document.createElement('li');
 
-        li.classList.add('cities-list__string');
+        li.classList.add('cities-list__city');
         li.textContent = city;
 
         ul.appendChild(li);
@@ -28,47 +28,26 @@ function createCitiesList(entities) {
 
 // конец не моего кода
 
-
-
-// reaction
-// class toggle
-// dom manipulation
-
-const input = document.querySelector('.search-form');
-const cities = document.querySelector('.cities');
-
-// focus
-// blur
-// change
-// keyup/keydown/keypress
-// click/mousedown/mouseup
+const input = document.querySelector('.search-form__input');
+const cities = document.querySelector('.search-form__popup');
 
 function onKeypress(event) {
     setTimeout(() => {
-        /*
-        1. получить ввод пользователя
-        2. по этому вводу найти города
-        3. по этим городам построить html-список
-        4. очистить текущий список
-        5. вставить в него новый список
-        */
-
         const searchString = input.value;
         const searchResult = search(searchString, 5);
         const searchList = createCitiesList(searchResult);
         cities.textContent = '';
         cities.appendChild(searchList);
-        console.log(searchList);
 
-        cities.classList.remove('hidden');
+        cities.classList.remove('search-form__popup_hidden');
     });
 }
 
 function onStringClick(event) {
     input.value = event.target.textContent;
-    cities.classList.add('hidden');
+    cities.classList.add('search-form__popup_hidden');
     input.focus();
 }
 
-input.addEventListener('keypress', onKeypress, true);
+input.addEventListener('keydown', onKeypress, true);
 cities.addEventListener('click', onStringClick);
